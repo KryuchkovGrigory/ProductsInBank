e48275836892f2e9b1ddd98a8a97a0136ab6ae57
Kurohtina Veronika <kurohtina.veronika@yandex.ru>
2023-05-02 18:53:17 +0300

    First commit

---
+++ src/main/java/bank/AvailableCurrency.java
@@ -0,0 +1,8 @@
+package bank;
+
+public enum AvailableCurrency
+{
+    USD,
+    RUB,
+    EUR
+}
---
+++ src/main/java/bank/Passport.java
@@ -0,0 +1,16 @@
+package bank;
+
+public class Passport
+{
+    private String firstName;
+    private String lastName;
+
+    private int age;
+
+    public Passport(String firstName, String lastName, byte age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+}
